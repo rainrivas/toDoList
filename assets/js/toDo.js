5f95983895925999a7bf66bc5b13f729dab22d7a
Rain <rainrivas@gmail.com>
2016-12-19 22:41:33 -0700

    Added event listeners for LI elements to strike through then and added event listener for +ToDo and added show/hide functionality

--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -1,20 +1,33 @@
 console.log("Connected");
 var newToDo;
+var addToDo = $("#addToDo");
+var toDoEntry = $("#toDoEntry");
+
+addToDo.on("click", function() {
+    toDoEntry.toggleClass("hidden");
+    console.log("+ ToDo clicked");
+});
+
+// This could become a "create new to do function which is called upon keypress of +ToDo button"
+toDoEntry.on("keypress", function(event) {
+    if (event.which === 13) {
+        console.log("You Pressed Enter; I should do something");
+        newToDo = "<li><span>X </span>" + toDoEntry.val() + "</li>";
+        $(newToDo).appendTo($("ul"));
+        toDoEntry.val("");
+        toDoEntry.toggleClass("hidden")
+            // once new list added and field cleared, we need to hide the to do list agian
+    }
+});
+
+$("li").on("click", function() {
+    $(this).toggleClass("completed");
+});
+
 $("li span").on("mouseenter", function() {
     $(this).css("font-weight", "bold");
 });
 
 $("li span").on("mouseleave", function() {
     $(this).css("font-weight", "normal");
-});
-
-// This could become a "create new to do function which is called upon keypress of +ToDo button"
-$("#newToDo").on("keypress", function(event) {
-    if (event.which === 13) {
-        console.log("You Pressed Enter; I should do something");
-        newToDo = "<li><span>X</span>" + $("#newToDo").val() + "</li>";
-        $(newToDo).appendTo($("ul"));
-        $("#newToDo").val("");
-        // once new list added and field cleared, we need to hide the to do list agian
-    }
 });
